47877f788664e088737e6ecd4989f0f4e14a7219
PaulG <paulgreecu75@gmail.com>
2019-04-30 13:11:22 +0200

    Update Receiver_Code.ino

--- NRF test/Receiver_Code/Receiver_Code.ino
+++ NRF test/Receiver_Code/Receiver_Code.ino
@@ -11,19 +11,19 @@
 #include <RF24.h>
 #include <Servo.h>
 
-#define button 4
+//#define button 4
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte addresses[][6] = {"00001", "00002"};
 Servo myServo;
-boolean buttonState = 0;
+//boolean buttonState = 0;
 
 void setup() {
   pinMode(button, INPUT);
   myServo.attach(5);
   radio.begin();
-  radio.openWritingPipe(addresses[0]); // 00001
+//  radio.openWritingPipe(addresses[0]); // 00001
   radio.openReadingPipe(1, addresses[1]); // 00002
   radio.setPALevel(RF24_PA_MIN);
   Serial.begin(9600);
@@ -39,9 +39,9 @@ void loop() {
       myServo.write(angleV);
     }
     delay(5);
-    radio.stopListening();
+/*    radio.stopListening();
     buttonState = digitalRead(button);
-    radio.write(&buttonState, sizeof(buttonState));
+    radio.write(&buttonState, sizeof(buttonState));*/
     Serial.println("Connected");
   }else{
     Serial.println("Not connected");
