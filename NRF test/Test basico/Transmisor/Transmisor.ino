ccdc2baf3cc04d00aaff7110349e7aa77f464b8d
PaulG <paulgreecu75@gmail.com>
2019-04-30 18:22:53 +0200

    Update Transmisor.ino

--- NRF test/Test basico/Transmisor/Transmisor.ino
+++ NRF test/Test basico/Transmisor/Transmisor.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+ 
+const int pinCE = 7;
+const int pinCSN = 8;
+RF24 radio(pinCE, pinCSN);
 
+struct pack{
+  int Y=0;
+};
+ 
+// Single radio pipe address for the 2 nodes to communicate.
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+ 
+//char data[16]="Hola mundo" ;
+
+typedef struct pack Package;
+Package data;
+ 
+void setup(void)
+{
+   radio.begin();
+   radio.openWritingPipe(pipe);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop(void)
+{
+   data.Y=analogRead(A0);
+   radio.write(&data, sizeof data);
+   delay(10);
 }
