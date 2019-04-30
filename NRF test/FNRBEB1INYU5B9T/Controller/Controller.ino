6b88c6d020eb9ca0fba1344fa5a72b9ad80be3ad
PaulG <paulgreecu75@gmail.com>
2019-04-30 15:12:51 +0200

    Test1

---
+++ NRF test/FNRBEB1INYU5B9T/Controller/Controller.ino
@@ -0,0 +1,66 @@
+/*
+ * If you use the serial monitor for debugging remember to ster the baud rate to 115200.
+ * The modules are set to only one-way communication. That means that the controller can only transmit and the tank can only receive.
+ * If you wish to change this for two-way communication there is plenty of documentation to do so on the internet and in examples.
+ * 
+ * This program reads the analog input of the joystick and sends it in packets to te tank.
+ */
+#include <SPI.h>  
+#include "RF24.h"
+
+RF24 myRadio (7, 8);
+byte addresses[][6] = {"0"};
+
+struct package
+{
+  int X=1;
+  int Y=1;
+  int Z=1;
+  int W=1;
+  int B = 0;
+};
+
+
+typedef struct package Package;
+Package data;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+  myRadio.begin();  
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openWritingPipe( addresses[0]);
+  delay(100);
+  pinMode(2, INPUT);
+}
+
+void loop()
+{
+ 
+  myRadio.write(&data, sizeof(data)); 
+
+  Serial.print("X:");
+  Serial.print(data.X);
+  Serial.print("    Y");
+  Serial.println(data.Y);
+  Serial.print("    Z");
+  Serial.println(data.Z);
+  Serial.print("    W");
+  Serial.println(data.W);
+  Serial.print("    B");
+  if (data.B == 1) {
+    Serial.println("1");
+  } else {
+    Serial.println("0");
+  }
+  data.X = analogRead(A0);
+  data.Y = analogRead(A1);
+  data.Z = analogRead(A2);
+  data.W = analogRead(A3);
+  data.B = digitalRead(2);
+  delay(100);
+}
---
+++ NRF test/FNRBEB1INYU5B9T/Tank/Tank.ino
@@ -0,0 +1,91 @@
+/*
+ * If you use the serial monitor for debugging remember to ster the baud rate to 115200.
+ * The modules are set to only one-way communication. That means that the controller can only transmit and the tank can only receive.
+ * If you wish to change this for two-way communication there is plenty of documentation to do so on the internet and in examples.
+ * 
+ * This program receives the X and Y values and processes it into movement.
+ */
+#include <SPI.h>  
+#include "RF24.h"
+#include <Servo.h> 
+
+RF24 myRadio (7, 8); 
+struct package
+{
+  int X=512;
+  int Y=512;
+  int Z=512;
+  int W=512;
+  int B=0;
+};
+
+byte addresses[][6] = {"0"}; 
+int OUT1 = 5;
+int OUT2 = 6;
+int OUT3 = 9;
+int OUT4 = 10;
+
+
+typedef struct package Package;
+Package data;
+
+Servo myServo;
+//boolean buttonState = 0;
+int angleV = 0;
+
+void setup() 
+{
+  Serial.begin(115200);
+  delay(1000);
+  myServo.attach(5);
+  myRadio.begin(); 
+  myRadio.setChannel(115); 
+  myRadio.setPALevel(RF24_PA_MAX);
+  myRadio.setDataRate( RF24_250KBPS ) ; 
+  myRadio.openReadingPipe(1, addresses[0]);
+  myRadio.startListening();
+  pinMode(OUT1, OUTPUT);
+  pinMode(OUT2, OUTPUT);
+  pinMode(OUT3, OUTPUT);
+  pinMode(OUT4, OUTPUT);
+  analogWrite(OUT1, 0);
+  analogWrite(OUT2, 0);
+  analogWrite(OUT3, 0);
+  analogWrite(OUT4, 0);
+}
+
+
+void loop()  
+{
+  if ( myRadio.available()) 
+  {
+    while (myRadio.available())
+    {
+      myRadio.read( &data, sizeof(data) );
+    }
+    Serial.print("X:");
+    Serial.print(data.X);
+    Serial.print("      Y");
+    Serial.println(data.Y);
+    Serial.print("      Z");
+    Serial.println(data.Z);
+    Serial.print("      W");
+    Serial.println(data.W);
+    Serial.print("      B");
+    Serial.println(data.B);
+    
+    int X = data.X;
+    int Y = data.Y;
+    int Z = data.Z;
+    int W = data.W;
+    int B = data.B;
+    
+    myRadio.read(&angleV, sizeof(angleV));
+     myServo.write(angleV);
+    
+    
+  } else {
+    Serial.println("Not Connected");
+  }
+
+}
