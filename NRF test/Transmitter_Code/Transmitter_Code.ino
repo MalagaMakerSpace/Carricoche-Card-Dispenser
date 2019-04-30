abddb08c9d7154d5f9b4a924994baa0622c07fba
PaulG <paulgreecu75@gmail.com>
2019-04-30 15:10:47 +0200

    Update Transmitter_Code.ino

--- NRF test/Transmitter_Code/Transmitter_Code.ino
+++ NRF test/Transmitter_Code/Transmitter_Code.ino
@@ -11,7 +11,7 @@
 #include <RF24.h>
 //#define led 12
 
-float datos[3];
+
 
 RF24 radio(7, 8); // CE, CSN
 
@@ -23,26 +23,25 @@ void setup() {
   radio.begin();
   radio.openWritingPipe(addresses[1]); // 00002
 //  radio.openReadingPipe(1, addresses[0]); // 00001
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
   Serial.begin(9600);
 }
 
 void loop() {
   delay(5);
-  datos[0]=analogRead(0)* (5.0 / 1023.0);;
-  datos[1]=millis();
-  datos[2]=3.14;
 //  radio.stopListening();
   int potValue = analogRead(A0);
   int angleValue = map(potValue, 0, 1023, 0, 180);
+  Serial.println(angleValue);
   radio.write(&angleValue, sizeof(angleValue));
-  bool ok = radio.write(datos, sizeof(datos));
-  Serial.print(angleValue);
+  bool ok = radio.write(&angleValue, sizeof(angleValue));
   if(ok){
-    Serial.println("/t Enviado");
+    Serial.println("\t mandado");
   }else{
-    Serial.println("/t NO Enviado");
+    Serial.println("\t no mandado");
   }
+  
   
 /*  delay(5);
   radio.startListening();
